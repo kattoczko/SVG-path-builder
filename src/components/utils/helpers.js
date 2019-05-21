5a64d4ff6b138330287bb4a35a8f87d67ecd6645
Katarzyna <kasia.oczkowska@gmail.com>
2019-05-21 17:06:51 +0800

    Updates

--- src/components/utils/helpers.js
+++
@@ -1,6 +0,0 @@
-export function positiveNumber(n) {
-  n = parseInt(n);
-  if (Number.isNaN(n) || n < 0) n = 0;
-
-  return n;
-}
--- src/index.js
+++
@@ -1,7 +0,0 @@
-import React from "react";
-import { render } from "react-dom";
-import "./styles/global/base.scss";
-
-import App from "./components/App";
-
-render(<App />, document.getElementById("app"));
